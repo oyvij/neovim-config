927bf4fccbb8342ba0d4f1b80d16e9bff49b71fd
Øyvind <oyvindj@stacc.com>
2025-03-29 12:15:58 +0100

    fix(main): Update commit hashes in lazy-lock.json and add multi-select functionality to telescope plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,11 +12,33 @@ return {
         layout_config = { prompt_position = "top", width = 0.9, height = 0.9 },
         sorting_strategy = "ascending",
         winblend = 0,
+        mappings = {
+          i = {
+            ["<CR>"] = function(prompt_bufnr)
+              local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+              local multi = picker:get_multi_selection()
+              if not vim.tbl_isempty(multi) then
+                require("telescope.actions").close(prompt_bufnr)
+                for _, j in pairs(multi) do
+                  if j.path ~= nil then
+                    if j.lnum ~= nil then
+                      vim.cmd(string.format("%s +%s %s", "edit", j.lnum, j.path))
+                    else
+                      vim.cmd(string.format("%s %s", "edit", j.path))
+                    end
+                  end
+                end
+              else
+                require("telescope.actions").select_default(prompt_bufnr)
+              end
+            end,
+          },
+        },
       },
     },
     keys = {
       {
-        "<leader>fg",
+        "<leader>/",
         "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
         desc = "Grep (root dir)",
       },
